96720b88c9a500c37d40547d9dfd8c9f55c8722d
Darin Boesch <darinboesch@me.com>
2018-01-13 22:04:45 -0500

    tweeked todo model to align properties

--- src/models/TodoModel.js
+++ src/models/TodoModel.js
@@ -2,10 +2,11 @@ import { observable } from "mobx";
 
 export default class TodoModel {
   id = Math.random();
-  @observable title;
+  @observable task;
+  @observable assignee;
   @observable finished = false;
 
-  constructor(title) {
-    this.title = title;
+  constructor(task) {
+    this.task = task;
   }
 }
